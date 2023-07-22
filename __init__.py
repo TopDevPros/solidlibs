160fcf94a06156ecbe4ec7321611ea13e5d389b3
SafeApps <3930611+safeapps@users.noreply.github.com>
2023-07-22 07:21:50 +0000

    Version 2.6.7

---
+++ __init__.py
@@ -0,0 +1,9 @@
+'''
+    solidlibs's open source libraries.
+
+    All files in this directory tree are open source,
+    licensed under GPLv3 <http://www.gnu.org/licenses/>.
+
+    Copyright 2009-2023 solidlibs
+    Last modified: 2023-05-17
+'''
---
+++ version.py
@@ -0,0 +1,11 @@
+'''
+    Current distributed version of the "solidlibs" package.
+
+    Copyright 2010-2023 solidlibs
+    Last modified: 2023-05-29
+
+'''
+
+CURRENT_VERSION = '2.6.7'
+COPYRIGHT = 'Copyright 2010-2023 solidlibs'
+LICENSE = 'GPLv3'
