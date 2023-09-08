be4b73511cb46e946a6e9ff7bc08466ab044e3e8
TopDevPros <support@topdevpros.com>
2023-09-08 09:06:10 +0000

    Split human readable times functions into its own module

--- python/times.py
+++ python/times.py
@@ -5,8 +5,8 @@
 
     Todo: Handle timezones better. See parse_timestamp(timezone=...),
 
-    Copyright 2009-2023 solidlibs
-    Last modified: 2023-05-17
+    Copyright 2009-2023 SolidLibs
+    Last modified: 2023-08-27
 
     This file is open source, licensed under GPLv3 <http://www.gnu.org/licenses/>.
 '''
@@ -15,7 +15,6 @@ import calendar
 from contextlib import contextmanager
 import re
 import time
-import human_readable
 # avoid conflict with 'timezone' param
 from datetime import timezone as datetime_timezone
 from datetime import date, datetime, timedelta
@@ -395,10 +394,10 @@ def parse_timestamp(timestr, startswith=False, default_year=None, timezone=None)
         'datetime.datetime(2019, 4, 11, 12, 49, tzinfo=datetime.timezone(datetime.timedelta(seconds=10800)))'
 
         >>> repr(parse_timestamp('Oct 28 09:54:48'))
-        'datetime.datetime(2022, 10, 28, 9, 54, 48)'
+        'datetime.datetime(2023, 10, 28, 9, 54, 48)'
 
         >>> repr(parse_timestamp(b'Oct 28 09:54:48'))
-        'datetime.datetime(2022, 10, 28, 9, 54, 48)'
+        'datetime.datetime(2023, 10, 28, 9, 54, 48)'
 
         >>> repr(parse_timestamp(b'2021-10-28'))
         'datetime.datetime(2021, 10, 28, 0, 0)'
@@ -832,240 +831,6 @@ def format_date(date=None):
         date = time.gmtime()
     return time.strftime('%Y-%m-%d', date)
 
-def seconds_human_readable(seconds):
-    '''
-        Formats seconds in a human readable form.
-
-        Args:
-            seconds: Seconds as a float or int.
-
-        Returns:
-            The years, months, weeks, days, minutes, hours, and remaining seconds
-            in human readable format (e.g., 48 minutes, 2 days, 5 hours, 6 minutes).
-
-        >>> seconds_in_week = seconds_in_day * 7
-        >>> seconds_in_year = seconds_in_week * 52
-        >>> current = datetime.utcnow()
-        >>> hour_ago = current - timedelta(minutes=48, seconds=20)
-        >>> seconds = (current - hour_ago).total_seconds()
-        >>> seconds_human_readable(seconds)
-        '48 minutes and 20 seconds'
-        >>> hour_ago = current - timedelta(minutes=60)
-        >>> seconds = (current - hour_ago).total_seconds()
-        >>> seconds_human_readable(seconds)
-        '1 hour'
-        >>> hours_ago = current - timedelta(hours=5, minutes=6)
-        >>> seconds = (current - hours_ago).total_seconds()
-        >>> seconds_human_readable(seconds)
-        '5 hours and 6 minutes'
-        >>> two_days_ago = current - timedelta(days=2)
-        >>> seconds = (current - two_days_ago).total_seconds()
-        >>> seconds_human_readable(seconds)
-        '2 days'
-        >>> five_plus_weeks_ago = current - timedelta(days=37)
-        >>> seconds = (current - five_plus_weeks_ago).total_seconds()
-        >>> seconds_human_readable(seconds)
-        '1 month and 6 days'
-        >>> eight_plus_years_ago = current - timedelta(days=2921)
-        >>> seconds = (current - eight_plus_years_ago).total_seconds()
-        >>> seconds_human_readable(seconds)
-        '8 years and 1 day'
-        >>> three_years_ago = current - timedelta(days=1095)
-        >>> seconds = (current - three_years_ago).total_seconds()
-        >>> seconds_human_readable(seconds)
-        '3 years'
-
-        >>> seconds_human_readable(0)
-        '0 seconds'
-
-        >>> seconds_human_readable(-1)
-        '11 months, 28 days, 23 hours, 59 minutes and 59 seconds'
-
-    '''
-
-    log.warning('human_readable() should be called directly. stacktrace below.')
-    log.stacktrace()
-
-    return human_readable.precise_delta(timedelta(seconds=seconds))
-
-    """ Let someone else maintain this. Delete if unused 2023-03-01
-    def format_unit_label(label, units):
-        ''' Format the label. '''
-        if units == 1:
-            status = f'{units} {label}'
-        else:
-            status = f'{units} {label}s'
-
-        return status
-
-    status = None
-
-    if seconds <= 0:
-        status = None
-
-    else:
-        seconds_in_week = seconds_in_day * 7
-        seconds_in_year = seconds_in_week * 52
-
-        years = seconds // seconds_in_year
-        if years > 0:
-            years_status = format_unit_label('year', int(years))
-            weeks = (seconds % seconds_in_year) // seconds_in_week
-            if weeks > 0:
-                weeks_status = format_unit_label('week', int(weeks))
-                status = f'{years_status}, {weeks_status}'
-            else:
-                status = years_status
-
-        if status is None:
-            weeks = seconds // seconds_in_week
-            if weeks > 0:
-                weeks_status = format_unit_label('week', int(weeks))
-                days = (seconds % seconds_in_week) // seconds_in_day
-                if days > 0:
-                    days_status = format_unit_label('day', int(days))
-                    status = f'{weeks_status}, {days_status}'
-                else:
-                    status = weeks_status
-
-        if status is None:
-            days = seconds // seconds_in_day
-            if days > 0:
-                days_status = format_unit_label('day', int(days))
-                hours = (seconds % seconds_in_day) // seconds_in_hour
-                if hours > 0:
-                    hours_status = format_unit_label('hour', int(hours))
-                    status = f'{days_status}, {hours_status}'
-                else:
-                    status = days_status
-
-        if status is None:
-            hours = seconds // seconds_in_hour
-            if hours > 0:
-                hours_status = format_unit_label('hour', int(hours))
-                minutes = (seconds % seconds_in_hour) // seconds_in_minute
-                if minutes > 0:
-                    minutes_status = format_unit_label('minute', int(minutes))
-                    status = f'{hours_status}, {minutes_status}'
-                else:
-                    status = hours_status
-
-        if status is None:
-            minutes = seconds // seconds_in_minute
-            if minutes > 1:
-                status = format_unit_label('minute', int(minutes))
-            else:
-                secs = seconds % seconds_in_minute
-                if secs > 0:
-                    status = format_unit_label('second', int(secs))
-
-    return status
-    """
-
-def timedelta_to_human_readable(td, verbose=True):
-    ''' Formats a timedelta in a human readable form.
-
-        Args:
-            td:      A timedelta produced by datetime.timedelta.
-            verbose: If True, includes full words for timeperiods (e.g., hour).
-                     If False, includes abbreviation for timeperiods (e.g., hr.).
-                     Optional. Defaults to True.
-
-        Returns:
-            A human readable form of the timedelta (e.g.,
-            2 days, 2 hours, 6 minutes, 3 seconds).
-            If total time is less than a second, then shows milliseconds
-            instead of microseconds. Otherwise, rounds to the nearest second.
-
-        >>> timedelta_to_human_readable(timedelta(days=1, seconds=123, minutes=4, hours=26))
-        '2 days, 2 hours, 6 minutes and 3 seconds'
-        >>> timedelta_to_human_readable(timedelta(seconds=123))
-        '2 minutes and 3 seconds'
-        >>> timedelta_to_human_readable(timedelta(seconds=65))
-        '1 minute and 5 seconds'
-        >>> timedelta_to_human_readable(timedelta(milliseconds=85))
-        '0.09 seconds'
-        >>> timedelta_to_human_readable(timedelta(days=1, seconds=123, minutes=4, hours=26), verbose=False)
-        '2d, 2h, 6m and 3s'
-        >>> timedelta_to_human_readable(timedelta(seconds=123), verbose=False)
-        '2m and 3s'
-        >>> timedelta_to_human_readable(timedelta(seconds=65), verbose=False)
-        '1m and 5s'
-        >>> timedelta_to_human_readable(timedelta(milliseconds=85), verbose=False)
-        '0.09s'
-    '''
-
-    log.warning('human_readable() should be called directly. stacktrace below.')
-    log.stacktrace()
-
-    if not verbose:
-        human_readable.i18n.activate('en_ABBR')
-    return human_readable.precise_delta(td)
-
-    """ Let someone else maintain this. Delete if unused 2023-03-01
-    tdString = ''
-
-    if td.days or td.seconds:
-
-        # days
-        if td.days:
-            tdString = f'{td.days} day{s_if_plural(td.days)}'
-
-        # round seconds
-        seconds = td.seconds
-        if (td.microseconds * 2) >= td.max.microseconds:
-            seconds = seconds + 1
-
-        # hours
-        hours = seconds // seconds_in_hour
-        if hours:
-            if tdString:
-                tdString = tdString + ', '
-            tdString = tdString + f'{hours} hour{s_if_plural(hours)}'
-
-        # minutes
-        secondsLeft = seconds - (hours * seconds_in_hour)
-        if secondsLeft:
-            minutes = secondsLeft // seconds_in_minute
-            if minutes:
-                if tdString:
-                    tdString = tdString + ', '
-                tdString = tdString + f'{minutes} minute{s_if_plural(minutes)}'
-                secondsLeft = secondsLeft - (minutes * seconds_in_minute)
-
-        # seconds
-        if secondsLeft:
-            if tdString:
-                tdString = tdString + ', '
-            tdString = tdString + f'{secondsLeft} second{s_if_plural(secondsLeft)}'
-
-    else:
-        milliseconds = (td.microseconds + 1) / 1000
-        tdString = f'{milliseconds} ms'
-
-    if not verbose:
-        m = re.match('.*( day)', tdString)
-        if m:
-            tdString = tdString.replace(m.group(1), ' day')
-
-        m = re.match('.*( hour)', tdString)
-        if m:
-            tdString = tdString.replace(m.group(1), ' hr')
-
-        m = re.match('.*( minute)', tdString)
-        if m:
-            tdString = tdString.replace(m.group(1), ' min')
-
-        m = re.match('.*( second)', tdString)
-        if m:
-            tdString = tdString.replace(m.group(1), ' sec')
-
-        tdString = tdString.replace(',', '')
-
-    return tdString
-    """
-
-
 def get_short_date_time(date_time):
     '''Format the date-time without seconds.
 
@@ -1332,8 +1097,13 @@ def date_range(start, end, lei_convention=False):
         [datetime.date(2012, 6, 1)]
     '''
 
+    log(f'start: {start}') #DEBUG
+    log(f'end: {end}') #DEBUG
+    log(f'lei_convention: {lei_convention}') #DEBUG
     increasing = start <= end
+    log(f'increasing: {increasing}') #DEBUG
     day = datetime_to_date(start)
+    log(f'day: {day}') #DEBUG
 
     if lei_convention:
         if increasing:
@@ -1346,6 +1116,18 @@ def date_range(start, end, lei_convention=False):
                 end = end + one_microsecond
             else:
                 end = end + one_day
+
+    end = datetime_to_date(end)
+
+    yield day
+    if increasing:
+        while day < end:
+            day = day + one_day
+            yield day
+    else:
+        while day > end:
+            day = day - one_day
+            yield day
 
     @contextmanager
     def wait(self, timeout):
