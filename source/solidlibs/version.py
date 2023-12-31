6b736d80f2cc3210d5aaae32bd1f1729669064c7
TopDevPros <support@topdevpros.com>
2023-12-31 07:11:18 +0000

    Update to 2.7.0

--- source/solidlibs/version.py
+++ source/solidlibs/version.py
@@ -2,10 +2,10 @@
     Current distributed version of the "solidlibs" package.
 
     Copyright 2010-2023 TopDevPros
-    Last modified: 2023-10-07
+    Last modified: 2023-12-25
 
 '''
 
-CURRENT_VERSION = '2.6.9'
+CURRENT_VERSION = '2.7.0'
 COPYRIGHT = 'Copyright 2010-2023 TopDevPros'
 LICENSE = 'GPLv3'
